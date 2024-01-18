b4af0cd3a7a09d6e2a93778ec99351d3c65140d1
Dhruv Aggarwal <dna@ucsb.edu>
2024-01-18 10:19:39 -0800

    maybe fd was wrong?

--- image_stream/src/image_publisher.cpp
+++ image_stream/src/image_publisher.cpp
@@ -3,12 +3,16 @@
 #include "sensor_msgs/msg/image.hpp"
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <ament_index_cpp/get_package_share_directory.hpp>
 
 class ImagePublisher : public rclcpp::Node
 {
 public:
     ImagePublisher() : Node("image_publisher")
     {
+        std::string package_share_directory = ament_index_cpp::get_package_share_directory("image_stream");
+        image_path_ = package_share_directory + "/resource/boat.png";
+
         publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image_topic", 10);
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100),
@@ -18,7 +22,7 @@ public:
 private:
     void timer_callback()
     {
-        cv::Mat image = cv::imread("resource/boat.png", cv::IMREAD_COLOR);
+        cv::Mat image = cv::imread(image_path_, cv::IMREAD_COLOR);
         if (image.empty()) {
             RCLCPP_ERROR(this->get_logger(), "Failed to load image 'resource/boat.png'");
             return;
@@ -30,6 +34,7 @@ private:
 
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
+    std::string image_path_;
 };
 
 int main(int argc, char *argv[])
